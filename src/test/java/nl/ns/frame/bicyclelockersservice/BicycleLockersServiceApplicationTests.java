e4ef62f3a2c54e250c72737b16c48b144849b369
mbt4nw1r <mbt4nw1r@gmail.com>
2022-02-20 14:56:39 +0100

    Initial commit

---
+++ src/test/java/nl/ns/frame/bicyclelockersservice/BicycleLockersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.ns.frame.bicyclelockersservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicycleLockersServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
